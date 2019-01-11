79d709b0ca09567ce00ce83e89930e74466b12c9
jccjd <1340944538@qq.com>
2019-01-11 12:38:18 +0800

    mvc learning

--- controller/single.php
+++ controller/single.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: lenovo
@@ -6,68 +7,107 @@
  * Time: 15:45
  * 单例模式
  */
-class SingletonV1 {
-    private static $_instance = null;
-    protected  static $_instance2 = null;
-
-    /*私有防止外面访问*/
+class Singleton0
+{
+    private static $instance = null;
+    private static $instance1 = null;
     private function __construct()
     {
     }
-    /*私有防止外面访问*/
+
     private function __clone()
     {
         // TODO: Implement __clone() method.
     }
-    //静态方法，单例统一访问
-    static public function getInstance() {
-        if ( is_null(self::$_instance) || isset(self::$_instance)) {
-            self::$_instance = new self();
+
+    /**
+     * @return null
+     */
+    static public function getInstance()
+    {
+        if (is_null(self::$instance) || isset(self::$instance)) {
+            self::$instance = new self();
         }
-        return self::$_instance;
+        return self::$instance;
     }
 
     /**
      * @return null
-     * v2
      */
-    public static function getInstance2()
+    public static function getInstance1()
     {
-        if (static::$_instance===null)
-        {
-            self::$_instance = new static;
+        if (static::$instance === null) {
+            static::$instance = new static;
         }
-        return self::$_instance2;
+        return self::$instance1;
+    }
+}
+class Singleton2 {
+    protected static $instace = null;
+
+    private function __construct()
+    {
     }
 
+    private function __clone()
+    {
+        // TODO: Implement __clone() method.
+    }
+
+    /**
+     * @return null
+     */
+    public static function getInstace()
+    {
+        if (static::$instace === null) {
+            static::$instace = new static;
+        }
+        return self::$instace;
+    }
+
+
 }
-abstract class Singleton {
+
+abstract class Singleton
+{
 
     final protected function __clone()
     {
         // TODO: Implement __clone() method.
     }
+
     final protected function __construct()
     {
         $this->init();
     }
-    protected function init(){}
-    public static function getInstance() {
+
+    protected function init()
+    {
+    }
+
+    public static function getInstance()
+    {
         if (static::$instance === null) {
             static::$instance = new static();
         }
         return static::$instance;
     }
 }
-class Base extends Singleton {
 
+class Base extends Singleton
+{
 }
-class A extends Base {
+
+class A extends Base
+{
     protected static $instance = null;
 }
-class B extends Base {
+
+class B extends Base
+{
     protected static $instance = null;
 }
+
 $c = A::getInstance();
 $d = B::getInstance();
 var_dump($c === $d);
