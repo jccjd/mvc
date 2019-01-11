fddcbf13e3fa4e4c9456e760c86d51da629c783d
jccjd <1340944538@qq.com>
2019-01-11 12:30:35 +0800

    mvc learning

--- controller/single.php
+++ controller/single.php
@@ -42,17 +42,32 @@ class SingletonV1 {
 
 }
 abstract class Singleton {
+
     final protected function __clone()
     {
         // TODO: Implement __clone() method.
     }
     final protected function __construct()
     {
+        $this->init();
     }
+    protected function init(){}
     public static function getInstance() {
         if (static::$instance === null) {
-            static::$instace = new static;
+            static::$instance = new static();
         }
         return static::$instance;
     }
 }
+class Base extends Singleton {
+
+}
+class A extends Base {
+    protected static $instance = null;
+}
+class B extends Base {
+    protected static $instance = null;
+}
+$c = A::getInstance();
+$d = B::getInstance();
+var_dump($c === $d);
